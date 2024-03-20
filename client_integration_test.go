635a9a7d6b9816cefd182b8da52c7be11f6f6519
tung.tq <tung.tq@teko.vn>
2024-03-20 17:39:11 +0700

    Add Client Create Request Integration Test

--- client_integration_test.go
+++ client_integration_test.go
@@ -50,3 +50,77 @@ func TestClientIntegration_Authenticate(t *testing.T) {
 		assert.Equal(t, int32(12_000), c.sessionTimeoutMs)
 	})
 }
+
+func (c *tcpConnTest) closeSession(client *Client) {
+	err := client.sendData(c, clientRequest{
+		xid:      client.nextXid(),
+		opcode:   opClose,
+		request:  &closeRequest{},
+		response: &closeResponse{},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	err = client.readSingleData(c)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestClientIntegration_Authenticate_And_Create(t *testing.T) {
+	t.Run("normal", func(t *testing.T) {
+		c, err := NewClient([]string{"localhost"}, 12*time.Second)
+		assert.Equal(t, nil, err)
+
+		netConn, err := net.Dial("tcp", "localhost:2181")
+		assert.Equal(t, nil, err)
+		defer func() { _ = netConn.Close() }()
+
+		connTest := &tcpConnTest{
+			conn: netConn,
+		}
+		defer connTest.closeSession(c)
+
+		// do authenticate
+		err = c.authenticate(connTest)
+		assert.Equal(t, nil, err)
+
+		req := clientRequest{
+			xid:    c.nextXid(),
+			opcode: opCreate,
+			request: &CreateRequest{
+				Path:  "/workers",
+				Data:  []byte("data01"),
+				Acl:   WorldACL(PermAll),
+				Flags: FlagEphemeral,
+			},
+			response: &createResponse{},
+		}
+
+		// Do Send
+		err = c.sendData(connTest, req)
+		assert.Equal(t, nil, err)
+
+		// Recv Response
+		err = c.readSingleData(connTest)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, 1, len(c.handleQueue))
+		assert.Equal(t, handleEvent{
+			state: StateHasSession,
+			req: clientRequest{
+				xid:    1,
+				opcode: opCreate,
+				request: &CreateRequest{
+					Path:  "/workers",
+					Data:  []byte("data01"),
+					Acl:   WorldACL(PermAll),
+					Flags: 1,
+				},
+				response: &createResponse{
+					Path: "/workers",
+				},
+			},
+		}, c.handleQueue[0])
+	})
+}
